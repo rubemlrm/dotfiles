bcb21a84af367c84bd857cb13ff22c95f9ee4fac
Rubem Mota <rubemmota89@gmail.com>
2020-07-14 21:33:42 +0100

    Possible fix for choco installer

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,19 @@
 $profileDir = Split-Path -parent $profile
+function Refresh-Environment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+    'HKCU:\Environment'
 
+    $locations | ForEach-Object {
+        $k = Get-Item $_
+        $k.GetValueNames() | ForEach-Object {
+            $name = $_
+            $value = $k.GetValue($_)
+            Set-Item -Path Env:\$name -Value $value
+        }
+    }
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
 
 Write-Host "Install powershell modules"
 #install posh
@@ -38,11 +52,19 @@ New-Item -Itemtype SymbolicLink -Path "$env:LocalAppData\Packages\Microsoft.Wind
 
 
 Write-Host "Installing Desktop Utilities..." -ForegroundColor "Yellow"
-if ((which cinst) -eq $null) {
-    iex (new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')
-    Refresh-Environment
-    choco feature enable -n=allowGlobalConfirmation
-}
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall
+# variable and importing the Chocolatey profile module.
+# Note: Using `. $PROFILE` instead *may* work, but isn't guaranteed to.
+$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+
+# refreshenv is now an alias for Update-SessionEnvironment
+# (rather than invoking refreshenv.cmd, the *batch file* for use with cmd.exe)
+# This should make git.exe accessible via the refreshed $env:PATH, so that it
+# can be called by name only.
+refreshenv
+choco feature enable -n=allowGlobalConfirmation
 
 # system and cli
 choco install curl                --limit-output
@@ -53,13 +75,16 @@ choco install python              --limit-output
 choco install ruby                --limit-output
 choco install bitwarden           --limit-output
 choco install filezilla           --limit-output
+choco install firefox           --limit-output
 choco install 7zip                --limit-output
 choco install php                 --limit-output
 choco install spotify             --limit-output
 choco install notepadplusplus.install --limit-output
+choco install vscode -y --limit-output
+choco install visualstudiocode-insiders -y --limit-output
 #fonts
 choco install sourcecodepro       --limit-output
-
+choco install meslolg.dz  --limit-output
 # browsers
 choco install GoogleChrome        --limit-output; <# pin; evergreen #> choco pin add --name GoogleChrome        --limit-output
 
@@ -67,18 +92,3 @@ choco install GoogleChrome        --limit-output; <# pin; evergreen #> choco pin
 Remove-Variable profileDir
 
 
-function Refresh-Environment {
-    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
-    'HKCU:\Environment'
-
-    $locations | ForEach-Object {
-        $k = Get-Item $_
-        $k.GetValueNames() | ForEach-Object {
-            $name = $_
-            $value = $k.GetValue($_)
-            Set-Item -Path Env:\$name -Value $value
-        }
-    }
-
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-}
