a18db990992533cf65775ee6566872da4fbad055
Rubem Mota <rubem.mota@socem.pt>
2021-06-11 11:32:39 +0100

    reset git cached files

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme Sorin
-
-$profileDir = Split-Path -parent $profile
-# load all 'autoload' scripts
-Get-ChildItem "${profileDir}\autoload\*.ps1" |  ForEach-Object -Process { .$_ }
