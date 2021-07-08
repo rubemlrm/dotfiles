22bbbc9e13cf67b40fb5984b50ebf83d6e0b9245
Rubem Mota <rubemmota89@gmail.com>
2021-07-08 15:38:05 +0100

    updated vim configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,7 +44,6 @@ cd "$USERHOME/.local/share/fonts" && curl -fLo 'Droid Sans Mono for Powerline Ne
 cd  "$CWD" || exit
 
 echo -e "\033[1;33m creating vim symlinks \033[0m"
-ln -s -f "$CWD/configs/vim/vimrc"  "$USERHOME/.vimrc"
 ln -s -f "$CWD/configs/vim/vim"  "$USERHOME/.vim"
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> bootstrap_install.logs 2>>bootstrap_error_install.logs
 vim +'PlugInstall' +qa
@@ -68,10 +67,6 @@ ln -s -f "$CWD/configs/kde/plasma-org.kde.plasma.desktop-appletsrc" "$USERHOME/.
 ln -s -f "$CWD/configs/kde/plasmarc" "$USERHOME/.config/plasmarc"
 ln -s -f "$CWD/configs/kde/plasmashellrc" "$USERHOME/.config/plasmashellrc"
 wget -O /home/rubemlrm/Pictures/wallhaven-8ogod1.jpg https://w.wallhaven.cc/full/8o/wallhaven-8ogod1.jpg
-
-echo -e "Restore latte settings"
-ln -s -f "$CWD/configs/latteDock/lattedockrc" "$USERHOME/.config/lattedockrc"
-ln -s -f "$CWD/configs/latteDock/latte/Default.layout.latte" "$USERHOME/.config/latte/Default.layout.latte"
 
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o "$USERHOME/bin/youtube-dl"
 chmod a+rx "$USERHOME/bin/youtube-dl"
