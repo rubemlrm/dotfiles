618359d58349b6e20164cceb3385d58f2e105011
Rubem Mota <rubemmota89@gmail.com>
2021-08-13 14:59:25 +0100

    update windows terminal settings to use the same font for every instance

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,6 +1,6 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme Sorin
+Set-PoshPrompt -Theme paradox
 
 $profileDir = Split-Path -parent $profile
 # load all 'autoload' scripts
