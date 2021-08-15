716cb8d371bc1d619bb409a5aea7a1374c1058c9
Rubem Mota <rubemmota89@gmail.com>
2021-08-15 11:25:30 +0100

    updated kde settings

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -22,6 +22,7 @@ alias ve='vim ~/.vimrc'
 alias ze='vim ~/.zshrc'
 alias syncDotFiles=""
 alias vim='nvim'
+alias vi='nvim'
 #apps
 alias irssi='TERM=screen-256color irssi'
 alias checkBranchesToRemove='git branch -r | awk "{print $1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print $1}"'
