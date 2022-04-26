07b8138c1affff7e94f0c896dae1a650e64d095e
Rubem <rubemmota89@gmail.com>
2022-04-26 19:39:31 +0100

    feat(tweaked-polybar-scripts): Tweaked polybar scripts
    Tweaked polybar scripts because of icons
    
     Changed polybar layout to
    remove backgrounds from modules
    
     Updated polybar scripts

--- polybar/.config/polybar/scripts/redshift.sh
+++ polybar/.config/polybar/scripts/redshift.sh
@@ -4,6 +4,7 @@ if [ "$(pgrep -x redshift)" ]; then
     localization=$(curl -s "https://location.services.mozilla.com/v1/geolocate?key=geoclue" | jq -r '"\(.location.lat):\(.location.lng)"')
     temp=$(redshift -l "$localization" -p 2> /dev/null | grep temp | cut -d ":" -f 2 | tr -dc "[:digit:]")
     echo "$temp"
+
 else
     echo "NA"
 fi
--- polybar/.config/polybar/scripts/updates.sh
+++ polybar/.config/polybar/scripts/updates.sh
@@ -29,12 +29,12 @@ while true; do
     # when there are updates available
     # every 10 seconds another check for updates is done
     while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo "$UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo "$UPDATES"
+        if (( UPDATES > 50 )); then
+            echo "$%{F#ffff00}%{F-}"
+        elif (( UPDATES > 25 )); then
+            echo "$%{F#f00}%{F-}"
         else
-            echo "None"
+            echo ""
         fi
         sleep 10
         get_total_updates
@@ -43,7 +43,7 @@ while true; do
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every 30 min for new updates
     while (( UPDATES == 0 )); do
-        echo "None"
+        echo ""
         sleep 1800
         get_total_updates
     done
