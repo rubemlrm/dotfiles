23726da5ef2e21305885c8ccb6dea6ab1d11ba3b
Rubem Mota <rubemmota89@gmail.com>
2022-06-19 20:47:43 +0100

    refactor(removed-configs): Removed configs for bspwm/dunst/polybar

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,7 +42,7 @@ for item in $gtk4FilesToRemove; do
     rm -rf "$USERHOME/.config/gtk-4.0/$item"
 done
 
-stow -vSt "$HOME" stow zsh vim nvim tmux terminator git npm neofetch gtk-3.0 gtk-4.0 solaar alacritty picom polybar sxhkd dunst bspmw rofi networkmanager-dmenu wallpapers kde
+stow -vSt "$HOME" stow zsh vim nvim tmux terminator git npm neofetch gtk-3.0 gtk-4.0 solaar wallpapers kde
 echo -e "\033[1;33m creating vim symlinks \033[0m"
 pip3 install --user --upgrade pynvim
 vim +'PlugInstall' +qa
