651273bb6304abf2fdb379575ae048ad1d5af99e
Rubem <rubemmota89@gmail.com>
2023-08-12 18:55:24 +0100

    fix(i3): Monitor scripts change

---
+++ i3/.config/scripts/set-monitors.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+if [[ "$HOSTNAME" == "DojoLaptop" ]]; then
+    xrandr --auto
+	   xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal \
+           --output DP-1 --off --output HDMI-1 --off \
+           --output DP-2 --off \
+           --output DP-3 --off \
+           --output DP-4 --off
+   fi
+elif [[ "$HOSTNAME" == "DojoDesktop" ]]; then
+    xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+        --output DP-1 --off \
+        --output DP-2 --off \
+        --output DP-3 --off \
+        --output HDMI-0 --off \
+        --output DP-4 --mode 1920x1080 --pos 1920x0 --rotate normal \
+        --output DP-5 --off
+else
+    echo "dummy"
+fi
