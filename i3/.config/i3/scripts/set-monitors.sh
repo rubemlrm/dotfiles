d4263c412c0d701c6ff582f95f23d3946854c017
Rubem <rubemmota89@gmail.com>
2023-12-02 15:12:30 +0000

    fix(i3): fixed i3 setting desktop orientation

--- i3/.config/i3/scripts/set-monitors.sh
+++ i3/.config/i3/scripts/set-monitors.sh
@@ -10,10 +10,14 @@ if [[ "$HOSTNAME" == "DojoLaptop" ]]; then
 fi 
 
 if [[ "$HOSTNAME" == "DojoDesktop" ]]; then
-    xrandr --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \          --output DP-1 --off \ 
-          --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal \ 
-          --output DP-3 --off --output HDMI-0 --off \ 
-          --output DP-4 --off --output DP-5 --off
+
+    xrandr --output DP-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+        --output DP-1 --off \
+        --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal \
+        --output DP-3 --off \
+        --output HDMI-0 --off \
+        --output DP-4 --off \
+        --output DP-5 --off
 else
     echo "dummy"
 fi
