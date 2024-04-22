a6e3eb58875779f9c699b621d54859677b90815c
Rubem Mota <rubemmota89@gmail.com>
2024-04-22 21:58:50 +0100

    chore(nvim): Added harpoon2 plugin
    Added harpoon2 plugin
    Tweaked nvim keybindings
    
    Signed-off-by: Rubem Mota <rubemmota89@gmail.com>

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,3 +1,10 @@
 local function augroup(name)
   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
+
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars", "*.tftest.hcl"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
