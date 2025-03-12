22ec775a30678cb68b7ee0ee0613415f24512cda
Rubem Mota <rubemmota89@gmail.com>
2025-03-12 22:34:49 +0000

    refactor(structure): Changed from role to playbook
    Signed-off-by: Rubem Mota <rubemmota89@gmail.com>

--- files/bspmw/.config/bspwm/scripts/list-shortcuts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2002
-cat "$HOME/.config/sxhkd/sxhkdrc" | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/last{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -width 1000
